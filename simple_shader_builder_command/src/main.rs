09e4e8eeb1d7c55d2f966f43625787d7ed0550bc
tuguzT <timurka.tugushev@gmail.com>
2025-07-03 19:34:30 +0300

    Make builder crates do more work

--- simple_shader_builder_command/src/main.rs
+++ simple_shader_builder_command/src/main.rs
@@ -1,7 +1,21 @@
+use std::error::Error;
+
+use rspirv::{
+    binary::{parse_bytes, Disassemble},
+    dr::Loader,
+};
+
 const PATH: &str = env!("simple_shader.spv");
 const SHADER: &[u8] = include_bytes!(env!("simple_shader.spv"));
 
-pub fn main() {
+pub fn main() -> Result<(), Box<dyn Error>> {
     println!("Reading SPIR-V shader from {PATH}");
-    println!("Shader data length: {}", SHADER.len());
+
+    let mut loader = Loader::new();
+    parse_bytes(SHADER, &mut loader)?;
+
+    let module = loader.module();
+    println!("Shader disassembly:\n{}", module.disassemble());
+
+    Ok(())
 }
